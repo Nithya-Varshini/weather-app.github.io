32f02bf2bbc609d5f5bd136dc05ebec169b18826
Nithya Varshini <97864144+Nithya-Varshini@users.noreply.github.com>
2023-03-07 10:40:38 +0530

    commit_2

---
+++ src/api.js
@@ -0,0 +1,10 @@
+export const geoApiOptions = {
+	method: 'GET',
+	headers: {
+		'X-RapidAPI-Key': 'd54bedbf92msh79b573feb5147d1p1a69f5jsn2ddf6b27a49b',
+		'X-RapidAPI-Host': 'wft-geo-db.p.rapidapi.com'
+	}
+};
+
+export const GEO_API_URL = "https://wft-geo-db.p.rapidapi.com/v1/geo";
+
